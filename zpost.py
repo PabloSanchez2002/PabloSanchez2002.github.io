8911d8459d9e6e703653ab099b4df9177c456ddf
PabloS <pablosanchez2002@gmail.com>
2023-02-14 16:46:46 +0100

    update

--- zpost.py
+++ zpost.py
@@ -2,5 +2,8 @@ import os
 import sys
 
 os.system("git add .")
-os.system("git commit -m '" + sys.argv[1] + "'")
+if(len(sys.argv) == 1):
+    os.system("git commit -m 'update'")
+else:
+    os.system("git commit -m '" + sys.argv[1] + "'")
 os.system("git push")
