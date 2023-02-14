742d76e9c7c7261a84d938bba8d9e6a7924c5ccc
PabloS <pablosanchez2002@gmail.com>
2023-02-14 17:01:30 +0100

    update 02/14/2023

--- zpost.py
+++ zpost.py
@@ -1,9 +1,11 @@
+from datetime import datetime
 import os
 import sys
 
 os.system("git add .")
 if(len(sys.argv) == 1):
-    os.system("git commit -m 'update'")
+    os.system("git commit -m 'update " +
+              datetime.now().strftime("%m/%d/%Y") + "'")
 else:
     os.system("git commit -m '" + sys.argv[1] + "'")
 os.system("git push")
